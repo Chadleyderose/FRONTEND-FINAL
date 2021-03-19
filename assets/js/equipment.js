61b1b3b9d5f2a215d0f40e028d26bb7c9484eebe
chadley <chadleyderose1911201@gmail.com>
2021-03-19 09:12:10 +0200

    final update

---
+++ assets/js/equipment.js
@@ -0,0 +1,5 @@
+// fetch('https://whispering-savannah-30451.herokuapp.com/equipment/')
+// .then(res => res.json())
+// .then(data => {
+//     console.log(data);
+// })
---
+++ assets/js/kitchen.js
@@ -0,0 +1,5 @@
+// fetch('https://whispering-savannah-30451.herokuapp.com/kitchen/')
+// .then(res => res.json())
+// .then(data => {
+//     console.log(data);
+// })
