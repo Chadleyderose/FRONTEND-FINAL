004c4a0f7231ec1e14ccb25193e9a9be38caa776
Chadley De Rose <68062582+Chadleyderose@users.noreply.github.com>
2021-03-24 10:33:39 +0200

    progress

---
+++ assets/js/fetch.js
@@ -0,0 +1,52 @@
+function createUsers(){
+    const inputs = document.getElementsByTagName("input");
+  
+    fetch("http://127.0.0.1:5000/new_user/",{
+        method: 'POST',
+        body: JSON.stringify({
+            username: inputs[3].value,
+            password: inputs[4].value,
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      alert("User has been created");
+      document.getElementById("register").reset();
+      window.location.href="./index.html";
+    });
+  };
+
+
+
+
+let Users = [];
+
+fetch("http://127.0.0.1:5000/users/")
+    .then((response) =>response.json())
+    .then((data) => {
+    console.log(data);
+    Users = data;
+});
+
+function loginMain(){
+    let inputs = document.getElementsByTagName("input");
+    
+    let logName = inputs[0].value;
+    let logPass = inputs[1].value;
+
+    let log = Users.filter(Users => {
+        return Users.Username == logName && Users.Password == logPass ?true : false;
+    })
+
+    console.log(log);
+
+    if (log.length > 0) {
+        alert(logName + " have successfully logged in");
+        window.location.href = "./display.html";
+    }else{
+        alert("Please enter a valid username and password");
+    }
+}
